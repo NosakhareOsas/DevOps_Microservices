30acbca02dbb5fffac40fb35eec66f1284dfdc5d
Nosa Osagiede <106080091+NosakhareOsas@users.noreply.github.com>
2022-08-17 12:47:25 +0100

    Update upload_docker.sh

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=elysiantech/flaskapp:v1
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag elysiantech/flaskapp:v1 elysiantech/flaskapp:latest
+docker login -u elysiantech
 
 # Step 3:
 # Push image to a docker repository
+docker push elysiantech/flaskapp:latest
