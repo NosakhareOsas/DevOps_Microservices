41fdfdb8c69a1c6a5152fbf9d2667183d7650b12
Nosa Osagiede <106080091+NosakhareOsas@users.noreply.github.com>
2022-08-17 12:49:32 +0100

    Update run_docker.sh

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,15 +1,16 @@
+
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t nosa/python/flaskapp:v1 .
+docker build -t elysiantech/flaskapp:v1 .
 
-# Step 2: 
+# Step 2:
 # List docker images
 docker image ls
 
-# Step 3: 
+# Step 3:
 # Run flask app
-docker run -it -p 8000:80 nosa/python/flaskapp:v1
+docker run -it -p 8000:80 elysiantech/flaskapp:v1
