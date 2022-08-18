7445bf9228d1f3d0bfada4f68e55e6741e4b78e4
Nosakhare Osagiede <osagiedenosakhare@gmail.com>
2022-08-18 12:29:58 +0000

    update kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+# dockerpath=elysiantech/flaskapp:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+chown root:root /tmp/juju-mk*
+minikube kubectl -- run app --image=elysiantech/flaskapp:latest --port=80
 
 # Step 3:
 # List kubernetes pods
-
+chown root:root /tmp/juju-mk*
+minikube kubectl -- get pods
 # Step 4:
 # Forward the container port to a host
-
+chown root:root /tmp/juju-mk*
+sleep 100
+minikube kubectl -- port-forward app 8000:80
